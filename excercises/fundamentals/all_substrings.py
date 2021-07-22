552b7c82ffc9d89d786ff7024392e917f1ffe2ce
katarina <katarinarosiak@gmail.com>
2021-07-22 09:16:32 +0200

    exercises

---
+++ excercises/fundamentals/all_substrings.py
@@ -0,0 +1,27 @@
+# All Substrings
+# Write a method that returns a list of all substrings of a string. The returned list should be ordered by where in the string the substring begins. This means that all substrings that start at position 0 should come first, then all substrings that start at position 1, and so on. Since multiple substrings will occur at each position, the substrings at a given position should be returned in order from shortest to longest.
+
+# You may (and should) use the leading_substrings method you wrote in the previous exercise:
+
+# Examples:
+
+# Copy Code
+# substrings('abcde') == [
+#   'a', 'ab', 'abc', 'abcd', 'abcde',
+#   'b', 'bc', 'bcd', 'bcde',
+#   'c', 'cd', 'cde',
+#   'd', 'de',
+#   'e'
+# ]
+
+
+def substrings(char_sequence):
+    all_substr = [
+        char_sequence[i:j]
+        for i in range(len(char_sequence))
+        for j in range(i + 1, len(char_sequence) + 1)
+    ]
+    return all_substr
+
+
+print(substrings("abcde"))
