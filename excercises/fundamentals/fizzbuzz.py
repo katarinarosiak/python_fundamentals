38fd732167e0b4bb0d74746393c333e461a704e2
katarina <katarinarosiak@gmail.com>
2021-07-27 09:48:00 +0200

    exercises

---
+++ excercises/fundamentals/fizzbuzz.py
@@ -0,0 +1,22 @@
+# fizzbuzz
+# Write a method that takes two arguments: the first is the starting number, and the second is the ending number. Print out all numbers between the two numbers, except if a number is divisible by 3, print "Fizz", if a number is divisible by 5, print "Buzz", and finally if a number is divisible by 3 and 5, print "FizzBuzz".
+
+# Example:
+
+# Copy Code
+# fizzbuzz(1, 15) # -> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
+
+
+def fizzbuzz(start, end):
+    for i in range(start, end + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        else:
+            print(i)
+
+
+fizzbuzz(1, 15)
